03c0bc0a97a25b2281626e4ed799e1f174e9e868
hanhdo97 <dohanh12b@gmail.com>
2024-03-25 15:36:37 +0700

    Update

---
+++ src/app/pages/login-page/login-page.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { ReactiveFormsModule, FormControl, FormsModule, FormGroupDirective, NgForm, Validators } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { ErrorStateMatcher } from '@angular/material/core';
+import { MatIconModule } from '@angular/material/icon';
+
+
+/** Error when invalid control is dirty, touched, or submitted. */
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
+
+@Component({
+  selector: 'app-login-page',
+  standalone: true,
+  imports: [MatIconModule, MatFormFieldModule, FormsModule, MatCardModule, MatButtonModule, ReactiveFormsModule, MatInputModule],
+  templateUrl: './login-page.component.html',
+  styleUrl: './login-page.component.css'
+})
+export class LoginPageComponent {
+  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
+  passwordFormControl = new FormControl('', [Validators.required]);
+  matcher = new MyErrorStateMatcher();
+
+}
